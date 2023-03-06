a236928ae2e5cbf656ecfa68a72a4a8e5f7624ef
Gauri Patil <gauri.be0811@gmail.com>
2023-03-06 21:19:18 +0530

    Chp 2 - Added first Jasmine test (describe, it, expect, beforeEach, afterEach & matcher functions) for ContactClass

---
+++ website/src/app/first-test-file.spec.ts
@@ -0,0 +1,9 @@
+describe('My first test', () => {
+    it('Should test if true equals true', () => {
+        expect(true).toBe(true);
+    });
+
+    it('Should check the sum of two numbers 2 + 2 = 4', () => {
+        expect(2 + 2).toBe(4);
+    })
+} )
