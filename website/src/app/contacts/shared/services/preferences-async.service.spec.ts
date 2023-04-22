fb50e6d3d31502516ce86116e226a8cba89be6e6
Gauri Patil <gauri.be0811@gmail.com>
2023-04-22 23:58:15 +0530

    Chp 6 - Angular Service Testing - PreferenceService  Async - Added negative test case

--- website/src/app/contacts/shared/services/preferences-async.service.spec.ts
+++ website/src/app/contacts/shared/services/preferences-async.service.spec.ts
@@ -32,4 +32,16 @@ describe('PreferencesAsyncService', () => {
             expect(error).toBeUndefined();
         }))
     );
+
+    it('should throw an error if key is missing', fakeAsync(inject([PreferencesAsyncService],
+        (service: PreferencesAsyncService) => {
+            let result, error;
+            service.getPropertyAsync('')
+                .then(value => result = value)
+                .catch(err => error = err);
+            
+            flushMicrotasks();
+            expect(result).toBeUndefined();
+            expect(error).toEqual('getPropertyAsync requires a property name');
+        })))
 })
